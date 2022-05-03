5bface93cd825d927e543862fe7631e14f1a7313
luisrivas123 <luis.rivas@correounivalle.edu.co>
2022-05-03 16:35:16 -0500

    uso de useState ProductItem onClick

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../styles/ProductItem.scss';
 
 import addToCart from '@icons/bt_add_to_cart.svg';
 const prouctItem = 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="bike';
 
 const ProductItem = () => {
+	const [cart, setCart] = useState('');
+
+	const handleClick = () => {
+		setCart('Hola mundo');
+	}
+
 	return (
 		<div className="ProductItem">
 			<img src={prouctItem} alt="" />
@@ -13,9 +19,10 @@ const ProductItem = () => {
 					<p>$120,00</p>
 					<p>Bike</p>
 				</div>
-				<figure>
+				<figure onClick={handleClick}>
 					<img src={addToCart} alt="" />
 				</figure>
+				{cart}
 			</div>
 		</div>
 	);
